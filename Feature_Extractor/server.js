5c23b349889210be70c9ca42eea10fe3cf83c8f3
KaixiangYang6 <kaixiang.yang.work@gmail.com>
2022-04-26 15:19:29 +0100

    feature extractor

---
+++ Feature_Extractor/server.js
@@ -0,0 +1,5 @@
+const express = require("express"); //在server.js文件内调用express包
+const app = express();              //将其赋给变量
+const server = app.listen(3000);    //
+app.use(express.static("public"));  //加载静态文件，含有html、css、sketch文件的文件夹
+console.log("It works");
